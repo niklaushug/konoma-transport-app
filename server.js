327bbbb2e7cc03cba8e2bb8ad06beb88d753cec1
Niklaus Hug <nhug@tocco.ch>
2019-07-11 19:13:57 +0200

    request opendata api transport
    - use static querystring
    - get request based on https://nodejs.org/api/http.html#http_http_get_url_options_callback

--- server.js
+++ server.js
@@ -1,14 +1,52 @@
 const express = require('express')
+const http = require('http')
 
 const app = express()
 
-app.get('/api/getTransport', (req, res) => {
-  res.json('response of /api/getTransport')
+app.get('/api/getTransport', (req, expressResponse) => {
+  http.get('http://transport.opendata.ch/v1/locations?query=Baden', httpGetResponse => {
+    const {
+      statusCode
+    } = httpGetResponse
+    let error
+
+    if (statusCode !== 200) {
+      error = new Error(`Request Failed (${statusCode})`)
+    }
+
+    if (error) {
+      console.error(error.message)
+      // Consume response data to free up memory
+      httpGetResponse.resume()
+      return
+    }
+
+    httpGetResponse.setEncoding('utf8')
+
+    let rawData = ''
+    httpGetResponse.on('data', chunk => {
+      rawData += chunk
+    })
+
+    httpGetResponse.on('end', () => {
+      try {
+        const parsedData = JSON.parse(rawData);
+        expressResponse.json(parsedData.stations)
+      } catch (e) {
+        console.error(e.message)
+        expressResponse.json(e.message)
+      }
+    })
+
+  }).on('error', (e) => {
+    console.error(`Got error: ${e.message}`)
+  })
+
 })
 
-app.get('*', (req, res) =>{
-  res.json('response of /*')
-});
+app.get('*', (req, expressResponse) =>{
+  expressResponse.json('response of /*')
+})
 
 const port = 5000
 app.listen(port)

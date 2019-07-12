45f32f6a78b01d0bd3ab7c353f93aff91768063e
Niklaus Hug <nhug@tocco.ch>
2019-07-12 19:16:24 +0200

    establish TableHeader and TableBody or show no result if list is empty

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
+const TableHeader = props =>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Id</th>
+      <th>Type</th>
+      <th>X</th>
+      <th>Y</th>
+    </tr>
+  </thead>
+
+const TableBody = ({list})=>
+  <tbody>
+    {list.map((item, i) =>
+      <tr>
+        <td>{item}</td>
+        <td>{item}</td>
+        <td>{item}</td>
+        <td>{item}</td>
+        <td>{item}</td>
+      </tr>
+    )}
+  </tbody>
+
+const App = () => {
+  const list = ['a', 'b']
+
   return (
     <div>
-      Test
+    {list.length ? (
+      <table>
+        <TableHeader />
+        <TableBody list={list} />
+      </table>
+    ): (
+      <span>no results</span>
+    )}
     </div>
-  );
+  )
 }
 
+
 export default App;
+
